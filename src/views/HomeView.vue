8d0c3edadff2a05a94ebf027ffa62e93ba4cf46d
Christoffer Jadermark <christoffer.jadermark@gmail.com>
2023-02-20 14:58:27 +0100

    Added cards to landing page with images and info.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap');
 
   .main-img {
+    margin-top: 3rem;
     width: 100%;
     height: auto;
   }
@@ -29,18 +30,38 @@
     font-size: 30px;
   }
 
+  #information-text {
+    font-family: 'Source Sans Pro', sans-serif;
+    text-transform: uppercase;
+    font-style: italic;
+    font-weight: 400;
+    color: #3a751c;
+    display: flex;
+    justify-content: center;
+    margin: 1.5em;
+  }
+
+  .card-group {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
   .btn {
     width: 10em;
     margin: auto;
   }
 
   @media (max-width: 408px) {
-    .log-in-container {
-      flex-direction: row;
-    }
-
     .btn-container {
       gap: 20px;
+    }
+  }
+
+  @media (max-width: 800px) {
+    .card-group {
+      align-items: center;
+      flex-direction: column;
     }
   }
 </style>
@@ -57,6 +78,52 @@
         >
         <b-button class="mx-1 btn" variant="outline-success">Logga in</b-button>
       </b-button-toolbar>
+    </div>
+    <h2 id="information-text">Aktuellt</h2>
+    <div>
+      <b-card-group class="card-group" deck>
+        <b-card
+          title="Protein Shakes"
+          img-src="assets/protein-img.jpg"
+          img-alt="Protein shake that can be made in store."
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          img-height="300px"
+        >
+          <b-card-text>
+            I kiosken kan du köpa våra extra goa protein shakes.
+          </b-card-text>
+        </b-card>
+        <b-card
+          title="Ta del av gym i närheten."
+          img-src="assets/pullup-img.jpg"
+          img-alt="Man that does a pullup."
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          img-height="300px"
+        >
+          <b-card-text>
+            Hos oss kan du ta del av flera olika gym i närheten av dig. För att
+            underlätta för just din träning.
+          </b-card-text>
+        </b-card>
+        <b-card
+          title="Tips & inspiration"
+          img-src="assets/protein-bar.jpg"
+          img-alt="Bild på en protein bar för inspiration till träningen."
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          img-height="300px"
+        >
+          <b-card-text>
+            Hos oss kan du boka en PT & kostrådgivare för inspiration till din
+            träning.
+          </b-card-text>
+        </b-card>
+      </b-card-group>
     </div>
   </main>
 </template>
