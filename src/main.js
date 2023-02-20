24a5d91918c007efdd124a8221ebd8f302130c89
Christoffer Jadermark <christoffer.jadermark@gmail.com>
2023-02-20 09:54:29 +0100

    Added Bootstrap-Vue-Next.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()]
+    })
+  ]
 })
