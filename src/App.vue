e02d11c616aab1e5d51c9e02e37ff4549bf66fa3
iiisachan <isa.chan@live.com>
2023-02-20 16:16:23 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,39 @@
 <script>
-  import CitiesList from './components/CitiesList.vue'
-  import CounterButton from './components/CounterButton.vue'
-  import HelloWorld from './components/HelloWorld.vue'
-
   export default {
-    components: {
-      CitiesList,
-      CounterButton,
-      HelloWorld
-    }
+    components: {}
   }
 </script>
 
 <template>
-  <nav>
-    <ul>
-      <li>
-        <RouterLink to="/">Hem</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/about">Om</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/contact">Kontakt</RouterLink>
-      </li>
-    </ul>
-  </nav>
-  <HelloWorld msg="Hello World!" />
-  <CitiesList />
-  <CounterButton />
+  <b-navbar fixed="top" toggleable="sm" variant="white">
+    <!-- <b-navbar-toggle target="navbar-collapse" /> -->
+
+    <b-navbar-toggle target="navbar-collapse">
+      <template #default="{ expanded }">
+        <font-awesome-icon v-if="expanded" icon="fa-regular fa-bars" />
+        <font-awesome-icon v-else icon="fa-regular fa-xmark" />
+      </template>
+    </b-navbar-toggle>
+
+    <b-collapse is-nav id="navbar-collapse">
+      <b-navbar-nav>
+        <b-nav-item to="/"> Hem </b-nav-item>
+        <b-nav-item to="/about"> Om </b-nav-item>
+        <b-nav-item to="/contact"> Kontakt </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+
   <main>
     <RouterView />
   </main>
 </template>
+
+<style>
+  * {
+    margin: 0;
+  }
+  body {
+    margin-top: 100px;
+  }
+</style>
