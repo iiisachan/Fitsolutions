cde4167f66fbfd5844f766d94c4365281ac3a26b
iiisachan <isa.chan@live.com>
2023-02-20 17:49:48 +0100

    added logo and icons

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,19 @@
 
 <template>
   <b-navbar fixed="top" toggleable="sm" variant="white">
-    <!-- <b-navbar-toggle target="navbar-collapse" /> -->
+    <b-navbar-brand to="/">
+      <img src="../assets/logo.png" alt="logo" />
+    </b-navbar-brand>
 
     <b-navbar-toggle target="navbar-collapse">
       <template #default="{ expanded }">
-        <font-awesome-icon v-if="expanded" icon="fa-regular fa-bars" />
-        <font-awesome-icon v-else icon="fa-regular fa-xmark" />
+        <font-awesome-icon v-if="expanded" icon="fa-solid fa-xmark" />
+        <font-awesome-icon v-else icon="fa-solid fa-bars" />
       </template>
     </b-navbar-toggle>
 
-    <b-collapse is-nav id="navbar-collapse">
-      <b-navbar-nav>
+    <b-collapse is-nav id="navbar-collapse" class="m-1">
+      <b-navbar-nav justified="true">
         <b-nav-item to="/"> Hem </b-nav-item>
         <b-nav-item to="/about"> Om </b-nav-item>
         <b-nav-item to="/contact"> Kontakt </b-nav-item>
@@ -30,10 +32,7 @@
 </template>
 
 <style>
-  * {
-    margin: 0;
-  }
-  body {
-    margin-top: 100px;
+  .navbar-toggler {
+    border: none;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faXmark)
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
@@ -7,4 +15,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')
