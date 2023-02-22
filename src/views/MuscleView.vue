63faca6c4f40c8828ad6a64a48771aa8f30f6f29
iiisachan <isa.chan@live.com>
2023-02-22 11:23:54 +0100

    updated muscle page

--- src/views/MuscleView.vue
+++ src/views/MuscleView.vue
@@ -4,43 +4,83 @@
   export default {}
 </script>
 
+<!-- TO DO: upddatera länkarna till övningarna -->
+<!-- TO DO: generera random länk  -->
+
 <template>
-  <div class="container">
+  <h1 class="title">Muskelgrupper</h1>
+  <div class="card-container">
     <b-card
-      title="Biceps"
-      img-src="../../assets/back.png"
+      title="Random"
+      img-src="../../assets/shuffle.png"
       img-alt="back logo"
       img-top
-      tag="article"
-      style="max-width: 10rem"
-      class="mb-2"
+      tag="RouterLink"
+      to=""
     />
     <b-card
       title="Rygg"
-      img-src="../../assets/biceps.png"
+      img-src="../../assets/back.png"
+      img-alt="back logo"
+      img-top
+      tag="RouterLink"
+      to=""
+    />
+    <b-card
+      title="Arm"
+      img-src="../../assets/biceps2.png"
       img-alt="biceps logo"
       img-top
-      tag="article"
-      style="max-width: 10rem"
-      class="mb-2"
+      tag="RouterLink"
+      to=""
     />
     <b-card
       title="Ben"
-      img-src="../../assets/quadricep.png"
+      img-src="../../assets/quadricep2.png"
       img-alt="ben logo"
       img-top
-      tag="article"
-      style="max-width: 10rem"
-      class="mb-2"
+      tag="RouterLink"
+      to=""
     />
     <b-card
       title="Mage"
       img-src="../../assets/upper-body.png"
       img-alt="upper-body logo"
       img-top
-      tag="article"
-      style="max-width: 10rem"
-      class="mb-2"
+      tag="RouterLink"
+      to=""
     />
   </div>
 </template>
+
+<style>
+  .card {
+    text-decoration: none;
+    color: black;
+    max-width: 250px;
+  }
+
+  .card-container {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    grid-gap: 20px;
+    justify-content: center;
+    justify-items: center;
+    margin: 3rem 20px;
+  }
+
+  .card-img-top {
+    padding: 1rem;
+  }
+
+  .title {
+    text-align: left;
+    margin: 2rem 2rem 4rem;
+  }
+
+  @media (min-width: 950px) {
+    .card-container {
+      grid-template-columns: repeat(4, minmax(0, 1fr));
+    }
+  }
+</style>
