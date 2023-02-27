e7bc94ff88bd925a5fee66e5090a340184d3557a
Christoffer Jadermark <christoffer.jadermark@gmail.com>
2023-02-27 14:40:48 +0100

    Added profile stuff

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,30 @@
 <script>
+  import axios from 'axios'
+
   export default {
-    components: {}
+    components: {},
+
+    data() {
+      return {
+        quotes: []
+      }
+    },
+    mounted() {
+      axios
+        .get('https://type.fit/api/quotes')
+        .then((response) => {
+          this.quotes = response.data
+          console.log(this.quotes)
+        })
+        .catch((error) => {
+          console.log(error)
+          this.errored = true
+        })
+    }
   }
 </script>
 
 <template>
   <h1>This is Profile Site</h1>
+  <p v-for="quote in quotes" :key="quote.id">{{ quote.text }}</p>
 </template>
