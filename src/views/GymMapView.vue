ffcffe1eaa35326b57c44f234aa12e906a9e193d
awaltherr <anton.walther@live.com>
2023-03-14 11:11:37 +0100

    Added feature to filter gyms by type in map

--- src/views/GymMapView.vue
+++ src/views/GymMapView.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <h1>Hitta Gym</h1>
+    <h1>Hitta Gym i Göteborg</h1>
     <div class="map-controls">
       <button @click="zoomIn">+</button>
       <button @click="zoomOut">-</button>
+      <select
+        v-model="selectedGymType"
+        @change="filterByGymType(selectedGymType)"
+      >
+        <option value="">Visa alla gym</option>
+        <option v-for="type in gymTypes" :value="type" :key="type">
+          {{ type }}
+        </option>
+      </select>
     </div>
     <div class="map" ref="map" />
   </div>
@@ -12,9 +21,11 @@
 <script>
   import mapboxgl from 'mapbox-gl'
   import gymData from '/gymData.json'
+
   export default {
     data() {
       return {
+        selectedGymType: '',
         map: null,
         mapboxToken:
           'pk.eyJ1IjoiYXdhbHRoZXJyIiwiYSI6ImNsZXlidzEwbDJqOGMzc3AxODZza2R5aDAifQ.BlU81_YGfwWReZNAgZmtpw'
@@ -32,7 +43,7 @@
           alert('Geolocation stöds inte på enheten')
         }
       },
-      showMap(longitude, latitude) {
+      showMap(longitude, latitude, filterByGymType) {
         mapboxgl.accessToken = this.mapboxToken
         this.map = new mapboxgl.Map({
           container: this.$refs.map,
@@ -41,7 +52,13 @@
           zoom: 10,
           scrollZoom: false
         })
-        gymData.forEach((gym) => {
+        let filteredGymData = gymData
+        if (filterByGymType) {
+          filteredGymData = gymData.filter(
+            (gym) => gym.type === filterByGymType
+          )
+        }
+        filteredGymData.forEach((gym) => {
           const gymMarker = new mapboxgl.Marker()
             .setLngLat([gym.longitude, gym.latitude])
             .addTo(this.map)
@@ -52,11 +69,26 @@
           )
         })
       },
+      filterByGymType(selectedGymType) {
+        this.map.remove()
+        this.showMap(
+          this.map.getCenter().lng,
+          this.map.getCenter().lat,
+          selectedGymType
+        )
+      },
       zoomIn() {
         this.map.zoomIn()
       },
       zoomOut() {
         this.map.zoomOut()
+      }
+    },
+    computed: {
+      gymTypes() {
+        const gymTypes = new Set()
+        gymData.forEach((gym) => gymTypes.add(gym.type))
+        return Array.from(gymTypes)
       }
     },
     mounted() {
